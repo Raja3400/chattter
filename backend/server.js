94228e6fe4df32eee2f651b7b0d0eceaf7eda2d4
Raja <baboo.raja.2015@gmail.com>
2024-04-29 15:15:02 +0530

    commit 4

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,13 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from './db/connectToMongoDB.js';
 import cookieParser from 'cookie-parser';
+import cors from "cors";
+import path  from 'path';
 const app=express();
 const PORT = process.env.PORT || 5000;
+//const __dirname = path.resolve();
 dotenv.config();
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.get("/",(req,res)=>{
@@ -16,6 +20,11 @@ app.get("/",(req,res)=>{
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// app.get("*", (req, res) => {
+// 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+// });
+
 
 app.listen(PORT,()=>{
     connectToMongoDB();
